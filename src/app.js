ad7d9e657a11483baac1e13934fb9e522fe9002b
Ivo Fernandes <ivofernand12@gmail.com>
2024-11-11 20:43:05 -0300

    update pre-deploy

--- src/app.js
+++ src/app.js
@@ -10,6 +10,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use("/api/reservas", reservaRoutes);
 app.use("/api/admin", adminRoutes);
 
